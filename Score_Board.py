362b1f428ebf03b48cf0def67a55fb767a6afe0a
Ronit <kumarronitnov26@gmail.com>
2025-08-16 16:07:56 +0530

    Add Highscore permanent

--- Score_Board.py
+++ Score_Board.py
@@ -1,12 +1,15 @@
 from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Times New Roman", 20, "normal")
+
 class ScoreBoard(Turtle):
+
     def __init__(self):
         super().__init__()
         self.score = 0
         self.color("white")
-        self.highscore = 0
+        with open("Data.txt") as data:
+            self.highscore = int(data.read())
         self.penup()
         self.goto(0, 270)
         self.update_score_board()
@@ -18,11 +21,12 @@ class ScoreBoard(Turtle):
     def increase_score(self):
         self.score += 1
         self.update_score_board()
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write("Game Over", align=ALIGNMENT, font=FONT)
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("Data.txt", "w") as data:
+                data.write(f"{self.highscore}")
+
         self.score = 0
         self.update_score_board()
+
