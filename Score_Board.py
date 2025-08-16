963df72eef952b3afc1b875352092fd49aab9156
Ronit <kumarronitnov26@gmail.com>
2025-08-16 14:37:33 +0530

    Add Highscore feature

--- Score_Board.py
+++ Score_Board.py
@@ -6,17 +6,23 @@ class ScoreBoard(Turtle):
         super().__init__()
         self.score = 0
         self.color("white")
+        self.highscore = 0
         self.penup()
         self.goto(0, 270)
         self.update_score_board()
         self.hideturtle()
     def update_score_board(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} \t High Score {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_score_board()
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("Game Over", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_score_board()
